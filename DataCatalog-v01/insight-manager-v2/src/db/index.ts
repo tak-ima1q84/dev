d6891d2af30e1b54dfcdaaa131068640e9409c43
tak-ima1q84 <testbed_1q84@outlook.com>
2025-12-08 19:09:54 +0900

    Delete DataCatalog-v01/insight-manager-v2 directory

--- DataCatalog-v01/insight-manager-v2/src/db/index.ts
+++
@@ -1,8 +0,0 @@
-import { drizzle } from 'drizzle-orm/postgres-js';
-import postgres from 'postgres';
-import * as schema from './schema';
-
-const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`;
-
-const client = postgres(connectionString);
-export const db = drizzle(client, { schema });
